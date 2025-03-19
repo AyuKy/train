e6bd9fa77f01befc8b908dea2afb1a21612e7eb9
Ayuky <3316079442@qq.com>
2025-03-19 23:01:14 +0800

    替换"Ayuky"logo,修改启动日志，修改访问接口/member

--- member/src/main/java/com/Ayuky/train/member/config/MemberApplication.java
+++ member/src/main/java/com/Ayuky/train/member/config/MemberApplication.java
@@ -1,14 +1,22 @@
 package com.Ayuky.train.member.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 
 @SpringBootApplication
 @ComponentScan("com.Ayuky.train.member.controller")
 public class MemberApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
     public static void main(String[] args){
-        SpringApplication.run(MemberApplication.class);
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("Successfully launch");
+        LOG.info("preview :\thttp://127.0.0.1:{}{}/test",env.getProperty("server.port"),env.getProperty("server.servlet.context-path"));
     }
 }
